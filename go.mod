6a054fbcde0bd4e3592505dceffd58fc7259e56a
hteen <hteen@qq.com>
2021-10-21 22:41:58 +0800

    .

--- go.mod
+++ go.mod
@@ -3,16 +3,19 @@ module valkbot
 go 1.17
 
 require (
+	github.com/PuerkitoBio/goquery v1.7.1
 	github.com/chromedp/cdproto v0.0.0-20211015214701-1037196e2fdd
 	github.com/chromedp/chromedp v0.7.4
 )
 
 require (
+	github.com/andybalholm/cascadia v1.2.0 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
 )
--- main.go
+++ main.go
@@ -1,152 +1,171 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/chromedp/cdproto/cdp"
-    "github.com/chromedp/cdproto/target"
-    "github.com/chromedp/chromedp"
-    "log"
-    "path/filepath"
-    "strings"
-    "time"
+	"context"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/cdproto/target"
+	"github.com/chromedp/chromedp"
+	"log"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-var nodes []*cdp.Node
-
 func main() {
-    // industry fence famous level unknown hungry about chief divide wait critic hockey
-    path, _ := filepath.Abs("./data")
-    // chromdp依赖context上限传递参数
-    ctx, _ := chromedp.NewExecAllocator(
-        context.Background(),
-        
-        // 以默认配置的数组为基础，覆写headless参数
-        // 当然也可以根据自己的需要进行修改，这个flag是浏览器的设置
-        append(
-            chromedp.DefaultExecAllocatorOptions[:],
-            chromedp.Flag("headless", false),
-            chromedp.Flag("disable-gpu", false),
-            chromedp.Flag("enable-automation", false),
-            chromedp.Flag("disable-extensions", false),
-            chromedp.UserDataDir(path),
-            chromedp.Flag("disable-sync", false),
-        )...,
-    )
-    
-    // 创建新的chromedp上下文对象，超时时间的设置不分先后
-    // 注意第二个返回的参数是cancel()
-    ctx, cancel := chromedp.NewContext(
-        ctx,
-        // 设置日志方法
-        chromedp.WithLogf(log.Printf),
-    )
-    
-    // 启动游戏
-    if err := chromedp.Run(ctx, LaunchGame()); err != nil {
-        log.Fatal(err)
-        return
-    }
-    
-    // 获取信息
-    if err := chromedp.Run(ctx, GetInfo()); err != nil {
-        log.Fatal(err)
-        return
-    }
-    
-    for i, _ := range nodes {
-    
-        // 获取信息
-        if err := chromedp.Run(ctx, GetNodeInfo(i)); err != nil {
-            log.Fatal(err)
-            return
-        }
-    }
-    
-    //time.Sleep(2 * time.Second)
-    //targets, _ := chromedp.Targets(ctx)
-    //for _, tg := range targets {
-    //    if strings.HasPrefix(tg.URL, "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html") {
-    //        newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(tg.TargetID))
-    //        sel := `#app-content`
-    //        var tit string
-    //        var val string
-    //        if err := chromedp.Run(newCtx, chromedp.Tasks{
-    //            chromedp.Text(sel, &val),
-    //            chromedp.Title(&tit),
-    //        }); err != nil {
-    //            log.Fatalln(err)
-    //        }
-    //
-    //        log.Println(tit)
-    //    }
-    //}
-    
-    chromedp.ListenTarget(ctx, func(ev interface{}) {
-        
-        //if reflect.TypeOf(ev).String() != "*cdproto.Message" {
-        //    log.Println(reflect.TypeOf(ev))
-        //}
-        
-        if tg, ok := ev.(*target.EventTargetInfoChanged); ok {
-            //t := page.HandleJavaScriptDialog(false)
-            log.Println(tg.TargetInfo.URL)
-            
-            if strings.HasPrefix(tg.TargetInfo.URL, "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html#connect") {
-                //log.Println("metamask link")
-                //sel := `#app-content > div > div.main-container-wrapper > div > div.permissions-connect-choose-account > div.permissions-connect-choose-account__footer-container > div.permissions-connect-choose-account__bottom-buttons > button.button.btn-primary`
-                //var tit string
-                var tit string
-                var text string
-                
-                newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(tg.TargetInfo.TargetID))
-                if err := chromedp.Run(newCtx, chromedp.Tasks{
-                    //chromedp.Title(&tit),
-                    chromedp.WaitVisible(`//button[text()='下一步']`),
-                    chromedp.Click(`//button[text()='下一步']`, chromedp.BySearch),
-                }); err != nil {
-                
-                }
-                
-                log.Println(22, tit, text)
-            }
-            
-        }
-    })
-    
-    select {}
-    cancel()
+	// industry fence famous level unknown hungry about chief divide wait critic hockey
+	path, _ := filepath.Abs("./data")
+	// chromdp依赖context上限传递参数
+	ctx, _ := chromedp.NewExecAllocator(
+		context.Background(),
+
+		// 以默认配置的数组为基础，覆写headless参数
+		// 当然也可以根据自己的需要进行修改，这个flag是浏览器的设置
+		append(
+			chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.Flag("headless", false),
+			chromedp.Flag("disable-gpu", false),
+			chromedp.Flag("enable-automation", false),
+			chromedp.Flag("disable-extensions", false),
+			chromedp.UserDataDir(path),
+			chromedp.Flag("disable-sync", false),
+		)...,
+	)
+
+	// 创建新的chromedp上下文对象，超时时间的设置不分先后
+	// 注意第二个返回的参数是cancel()
+	ctx, cancel := chromedp.NewContext(
+		ctx,
+		// 设置日志方法
+		chromedp.WithLogf(log.Printf),
+	)
+
+	// 启动游戏
+	//if err := chromedp.Run(ctx, LaunchGame()); err != nil {
+	//    log.Fatal(err)
+	//    return
+	//}
+
+	// 获取信息
+	for {
+
+		if err := chromedp.Run(ctx, GetInfo()); err != nil {
+			log.Fatal(err)
+			return
+		}
+		log.Println(11111)
+		time.Sleep(time.Second * 5)
+	}
+
+	//time.Sleep(2 * time.Second)
+	//targets, _ := chromedp.Targets(ctx)
+	//for _, tg := range targets {
+	//    if strings.HasPrefix(tg.URL, "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html") {
+	//        newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(tg.TargetID))
+	//        sel := `#app-content`
+	//        var tit string
+	//        var val string
+	//        if err := chromedp.Run(newCtx, chromedp.Tasks{
+	//            chromedp.Text(sel, &val),
+	//            chromedp.Title(&tit),
+	//        }); err != nil {
+	//            log.Fatalln(err)
+	//        }
+	//
+	//        log.Println(tit)
+	//    }
+	//}
+
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+
+		//if reflect.TypeOf(ev).String() != "*cdproto.Message" {
+		//    log.Println(reflect.TypeOf(ev))
+		//}
+
+		if tg, ok := ev.(*target.EventTargetInfoChanged); ok {
+			//t := page.HandleJavaScriptDialog(false)
+			log.Println(tg.TargetInfo.URL)
+
+			if strings.HasPrefix(tg.TargetInfo.URL, "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html#connect") {
+				//log.Println("metamask link")
+				//sel := `#app-content > div > div.main-container-wrapper > div > div.permissions-connect-choose-account > div.permissions-connect-choose-account__footer-container > div.permissions-connect-choose-account__bottom-buttons > button.button.btn-primary`
+				//var tit string
+				var tit string
+				var text string
+
+				newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(tg.TargetInfo.TargetID))
+				if err := chromedp.Run(newCtx, chromedp.Tasks{
+					//chromedp.Title(&tit),
+					chromedp.WaitVisible(`//button[text()='下一步']`),
+					chromedp.Click(`//button[text()='下一步']`, chromedp.BySearch),
+				}); err != nil {
+
+				}
+
+				log.Println(22, tit, text)
+			}
+
+		}
+	})
+
+	select {}
+	cancel()
 }
 
 // LaunchGame 启动游戏
 func LaunchGame() chromedp.Tasks {
-    return chromedp.Tasks{
-        chromedp.Navigate("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html"),
-        chromedp.WaitVisible(`#password`),
-        chromedp.SendKeys(`#password`, "11111111"),
-        chromedp.Sleep(time.Second),
-        chromedp.Click(`#app-content > div > div.main-container-wrapper > div > div > button`, chromedp.ByID),
-        chromedp.Navigate("https://game.playvalkyr.io/fighters"),
-        chromedp.WaitVisible(`#app > div.warning > div > button.el-button.el-button--block.el-button--success.el-button--large`),
-        chromedp.Click(`#app > div.warning > div > button.el-button.el-button--block.el-button--success.el-button--large`, chromedp.ByQuery),
-    }
+	return chromedp.Tasks{
+		chromedp.Navigate("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html"),
+		chromedp.WaitVisible(`#password`),
+		chromedp.SendKeys(`#password`, "11111111"),
+		chromedp.Sleep(time.Second),
+		chromedp.Click(`#app-content > div > div.main-container-wrapper > div > div > button`, chromedp.ByID),
+		chromedp.Navigate("https://game.playvalkyr.io/fighters"),
+		chromedp.WaitVisible(`#app > div.warning > div > button.el-button.el-button--block.el-button--success.el-button--large`),
+		chromedp.Click(`#app > div.warning > div > button.el-button.el-button--block.el-button--success.el-button--large`, chromedp.ByQuery),
+	}
 }
 
 func GetInfo() chromedp.Tasks {
-    return chromedp.Tasks{chromedp.WaitVisible(`#characters`),
-        chromedp.Nodes(`#characters div.character-info`, &nodes, chromedp.BySearch),
-    }
+	var nodes []*cdp.Node
+
+	return chromedp.Tasks{
+		chromedp.Navigate("https://v2ex.com/"),
+		chromedp.WaitVisible(`#Main`),
+		chromedp.Nodes(`#Main .item`, &nodes, chromedp.BySearch),
+		chromedp.ActionFunc(func(c context.Context) error {
+
+			for i, node := range nodes {
+				html, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(c)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				log.Println(i, doc.Find(`.item_title`).Text(), err)
+			}
+
+			return nil
+		}),
+	}
 }
 
-
 func GetNodeInfo(i int) chromedp.Tasks {
-    
-    var text string
-    return chromedp.Tasks{chromedp.WaitVisible(`#characters`),
-        chromedp.Text(fmt.Sprintf(`#characters div.character-info .level:eq(%d)`, i), &text, chromedp.BySearch),
-        chromedp.ActionFunc(func(ctx context.Context) error {
-            log.Println(text)
-            return nil
-        }),
-    }
+
+	var text string
+	return chromedp.Tasks{chromedp.WaitVisible(`#characters`),
+		chromedp.Text(fmt.Sprintf(`#characters div.character-info .level:eq(%d)`, i), &text, chromedp.BySearch),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			log.Println(text)
+			return nil
+		}),
+	}
 }
